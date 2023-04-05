8464b3de9b221fe9c0fe710bc467d67f6a53aa94
princekhj555 <princekhj555@gmail.com>
2023-04-05 21:21:00 +0530

    Designed Login page and Reister Page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,26 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+import { LoginComponent } from './pages/login/login.component';
+import { SignupComponent } from './pages/signup/signup.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path:"",
+    pathMatch:"full",
+    component:HomeComponent
+  },
+  {
+    path:"signup",
+    pathMatch:"full",
+    component:SignupComponent
+  },
+  {
+    path:"login",
+    pathMatch:"full",
+    component:LoginComponent
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
---
+++ src/app/pages/signup/signup.component.ts
@@ -0,0 +1,49 @@
+import { Component } from '@angular/core';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { UserService } from 'src/app/services/user.service';
+import Swal from 'sweetalert2';
+
+@Component({
+  selector: 'app-signup',
+  templateUrl: './signup.component.html',
+  styleUrls: ['./signup.component.css']
+})
+export class SignupComponent {
+  constructor(private userService:UserService,private snack:MatSnackBar){}
+  public user={
+    username:"",
+    password:"",
+    firstName:"",
+    lastName:"",
+    email:"",
+    phone:""
+
+  }
+  registerSubmit(){
+    if(this.user.username=="" || this.user.username==null ||
+        this.user.email==""||this.user.email==null||
+        this.user.firstName==""||this.user.firstName==null||
+        this.user.phone==""||this.user.phone==null||
+        this.user.password==""||this.user.password==null
+        ){
+      this.snack.open("Please fill the required fields !!","",{
+        duration:3000
+      });
+      return;
+    }
+    this.userService.addUser(this.user).subscribe(
+      (data)=>{
+        //success
+        console.log(data);
+        Swal.fire("Success","User is registered","success");
+      },
+      (error)=>{
+        console.log(error);
+        this.snack.open("Something went wrong !!","",{
+          duration:3000
+        });
+        //error
+      }
+      )
+  }
+}
